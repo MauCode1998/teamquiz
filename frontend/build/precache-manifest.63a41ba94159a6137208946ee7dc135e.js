d680037489208e9acea2aa323c0ed257fb1d73e7
MauCode1998 <mauricekalisch2@hotmail.de>
2025-06-06 12:27:26 +0200

    Pre-built Frontend hinzugefügt für einfaches Render Deployment

---
+++ frontend/build/precache-manifest.63a41ba94159a6137208946ee7dc135e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b59556a49941143dcb7ea367617b7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "632dc9ac2b6b6ad3d54a",
+    "url": "/static/css/main.b49eb66c.chunk.css"
+  },
+  {
+    "revision": "eca5e11f0a4ef517ef0b",
+    "url": "/static/js/2.001402cd.chunk.js"
+  },
+  {
+    "revision": "030cc1de2063089ef520e2f2b65b3363",
+    "url": "/static/js/2.001402cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b27f2bf65cdd3aa86b1e",
+    "url": "/static/js/3.4ba8a846.chunk.js"
+  },
+  {
+    "revision": "632dc9ac2b6b6ad3d54a",
+    "url": "/static/js/main.e9984bb9.chunk.js"
+  },
+  {
+    "revision": "1bc8d122bb68bb044223",
+    "url": "/static/js/runtime-main.51c6d17a.js"
+  },
+  {
+    "revision": "9595cc1f77c6679ce1e8313472795d81",
+    "url": "/static/media/logo.9595cc1f.png"
+  },
+  {
+    "revision": "bd134657a35a4fbe0f3a43834088924c",
+    "url": "/static/media/start.bd134657.jpeg"
+  },
+  {
+    "revision": "c6fc540a3e51bf13bd8771b3290f1d59",
+    "url": "/static/media/teamquizlogo.c6fc540a.jpeg"
+  }
+]);
